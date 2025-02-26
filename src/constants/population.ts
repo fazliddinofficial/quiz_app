5091c5d786f27e1003782b239ffced63e152e53f
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2025-02-26 18:33:23 +0500

    add: population

--- src/constants/population.ts
+++ src/constants/population.ts
@@ -5,7 +5,9 @@ export const POPULATIONS = {
     { path: "answers", model: SCHEMA_NAMES.Answer },
     { path: "quiz", model: SCHEMA_NAMES.Quiz },
   ],
-  quiz: [
-    { path: "questions", model: SCHEMA_NAMES.Question }
-  ]
+  quiz: [{ path: "questions", model: SCHEMA_NAMES.Question }],
+  game: [
+    { path: "playersId", model: SCHEMA_NAMES.User },
+    { path: "quizId", model: SCHEMA_NAMES.Quiz },
+  ],
 };
